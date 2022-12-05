c333e6a6300795abc34a137e5639c8a0462e340e
zenrfd <sunamonogatari@gmail.com>
2022-12-05 08:29:38 +0700

    Deploy gh-pages

--- _next/static/chunks/webpack-e974cc324f17ff08.js
+++ _next/static/chunks/webpack-e974cc324f17ff08.js
@@ -121,7 +121,7 @@
 /******/ 		// This function allow to reference all chunks
 /******/ 		__webpack_require__.miniCssF = function(chunkId) {
 /******/ 			// return url for filenames based on template
-/******/ 			return "static/css/" + "5566c30d531fb18e" + ".css";
+/******/ 			return "static/css/" + "7cf56b1f0f98fd85" + ".css";
 /******/ 		};
 /******/ 	}();
 /******/ 	
